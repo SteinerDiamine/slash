3284e5e34833ae8d915ca50bf71aaa12756887cc
SteinerDiamine <shubhamsharma68663@gmail.com>
2024-09-16 20:53:56 +0530

    next routing with folder strcture

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+
+    
+    <div>
+
+      <div className="bg-black text-white"><p>Hello world </p></div>
+      <div>
+      <Button  className="text-3xl">h</Button>
+      </div>
+      
+
+    </div>
+  )
+}
